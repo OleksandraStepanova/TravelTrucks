8fd0f8a3e9f1258174a7591dd106be101ffeeaaf
OleksandraStepanova <128746781+OleksandraStepanova@users.noreply.github.com>
2025-02-16 21:40:12 +0000

    Deploying to gh-pages from @ OleksandraStepanova/TravelTrucks@30501f30f870697bf6ccbbc6ee91a46bbe60bc4c 🚀

--- catalog.js
+++ catalog.js
@@ -6,63 +6,63 @@
                       <div class="price-campers">
                           <p>&euro;${e.price},00</p>
                           <button type="button" class="btn-like" data-id="${e.id}">
-                              <img class='img-like' src="/img/heart.svg" alt="like" />
+                              <img class='img-like' src='heart.svg' alt="like" />
                           </button>                                                      
                       </div>
                   </div>
                   <div class="wrapper-campers">
                       <div class="reviews-campers">
-                        <img src='./img/star.svg' alt="star" />
+                        <img src='star.svg' alt="star" />
                         <p>${e.rating}</p>
                         <p>(${e.reviews.length} Reviews)</p>
                       </div>
                       <div class="reviews-campers">
-                        <img src='./img/map.svg' alt="location" />
+                        <img src='map.svg' alt="location" />
                         <p>${e.location}</p>
                       </div>
                   </div>                    
                   <p class="description-campers">${e.description}</p>
                   <ul class="characteristics-list">
                       <li class="characteristics-item">
-                          <img src='./img/diagram.svg' alt="diagram" />
+                          <img src='diagram.svg' alt="diagram" />
                           <p class='characteristics'>${e.transmission}</p>
                       </li>
                       <li class='characteristics-item'>
-                          <img src='./img/fuel-pump.svg' alt="fuel" />
+                          <img src='fuel-pump.svg' alt="fuel" />
                           <p class='characteristics'>${e.engine}</p>
                       </li>                        
                       ${e.kitchen?`
                       <li class='characteristics-item'>
-                          <img src='./img/cup-hot.svg' alt="kitchen" />
+                          <img src='cup-hot.svg' alt="kitchen" />
                           <p class='characteristics'>Kitchen</p>
                       </li>`:""}
                       
                       ${e.AC?`
                       <li class='characteristics-item'>
-                          <img src='./img/wind.svg' alt="AC" />
+                          <img src='wind.svg' alt="AC" />
                           <p class='characteristics'>AC</p>
                       </li>`:""}
                       
                       ${e.bathroom?`
                       <li class='characteristics-item'>
-                          <img src='./img/bi_droplet.svg' alt="droplet" />
+                          <img src='bi_droplet.svg' alt="droplet" />
                           <p class='characteristics'>Bathroom</p>
                       </li>`:""}
                       
                       ${e.radio?`
                       <li class='characteristics-item'>
-                          <img src='./img/ui-radios.svg' alt="radio" />
+                          <img src='ui-radios.svg' alt="radio" />
                           <p class='characteristics'>Radio</p>
                       </li>`:""}
                        ${e.TV?`
                       <li class='characteristics-item'>
-                          <img src='./img/tv.svg' alt="tv" />
+                          <img src='tv.svg' alt="tv" />
                           <p class='characteristics'>TV</p>
                       </li>`:""}
                   </ul>                    
                   <a class="campers-link" data-id="${e.id}">Show more</a>                    
               </div>
-          </li>`}const h=document.querySelector(".result"),q=document.querySelector(".load-more");function y(e,t){h.innerHTML=e.slice(0,t).map(v).join("");const i=JSON.parse(localStorage.getItem("likedCampers"))||{};document.querySelectorAll(".btn-like").forEach(r=>{const s=r.dataset.id;i[s]&&(r.classList.add("liked"),r.querySelector("img").src="./src/img/red-heart.svg")})}function b(e,t,i){h.insertAdjacentHTML("beforeend",e.slice(t,i).map(v).join(""))}function $(e,t,i){q.style.display=e*t>=i.length?"none":"block"}function E(e){var s,a;const t=((s=document.querySelector("#location"))==null?void 0:s.value.toLowerCase())||"",i=[...document.querySelectorAll(".container-filters.active-filter")].map(c=>c.id),r=((a=document.querySelector('input[name="vehicleType"]:checked'))==null?void 0:a.value)||"";return e.filter(c=>{const L=t?c.location.toLowerCase().includes(t):!0,w=i.every(u=>c[u]===!0||c[u]==="automatic"),S=r?c.form===r:!0;return L&&w&&S})}function M(e){return`
+          </li>`}const h=document.querySelector(".result"),q=document.querySelector(".load-more");function y(e,t){h.innerHTML=e.slice(0,t).map(v).join("");const i=JSON.parse(localStorage.getItem("likedCampers"))||{};document.querySelectorAll(".btn-like").forEach(r=>{const s=r.dataset.id;i[s]&&(r.classList.add("liked"),r.querySelector("img").src="./src/img/red-heart.svg")})}function b(e,t,i){h.insertAdjacentHTML("beforeend",e.slice(t,i).map(v).join(""))}function $(e,t,i){q.style.display=e*t>=i.length?"none":"block"}function E(e){var s,a;const t=((s=document.querySelector("#location"))==null?void 0:s.value.toLowerCase())||"",i=[...document.querySelectorAll(".container-filters.active-filter")].map(c=>c.id),r=((a=document.querySelector('input[name="vehicleType"]:checked'))==null?void 0:a.value)||"";return e.filter(c=>{const L=t?c.location.toLowerCase().includes(t):!0,w=i.every(m=>c[m]===!0||c[m]==="automatic"),S=r?c.form===r:!0;return L&&w&&S})}function M(e){return`
     <section class='camp'>
       <h2 class='title-camp'>${e.name}</h2>
       <div class='wrapper-camp'>
@@ -96,5 +96,5 @@
       </div>
       <img class='close-modal' src="close.svg" alt="close" width='22px' />
     </section>
-    `}async function I(e){try{const t=await C(e);if(!t){console.error("Camper not found");return}const i=document.querySelector(".modal-content");if(!i){console.error("Modal content not found in DOM");return}i.innerHTML=M(t),document.querySelector(".modal").classList.add("open"),addNavigationHandlers()}catch(t){console.error("Error fetching camper data:",t)}}function T(){document.querySelector(".modal").classList.remove("open")}document.addEventListener("click",function(e){(e.target.classList.contains("close-modal")||e.target.classList.contains("modal"))&&T()});document.addEventListener("click",function(e){if(e.target.closest(".btn-like")){const t=e.target.closest(".btn-like"),i=t.dataset.id;t.classList.toggle("liked");const r=t.querySelector("img"),s=t.classList.contains("liked");r.src=s?"./img/red-heart.svg":"./img/heart.svg";const a=JSON.parse(localStorage.getItem("likedCampers"))||{};a[i]=s,localStorage.setItem("likedCampers",JSON.stringify(a))}});const O=document.querySelector(".result"),A=document.querySelector(".loader"),p=document.querySelector(".load-more"),F=document.querySelector("#filtersForm");A.style.display="none";p.style.display="none";let l=1;const n=4;let m=[],o=[];async function N(){try{m=await k(),o=m,y(o,n),l=1,$(l,n,o)}catch{O.innerHTML="<p>Not found</p>",p.classList.add("no-active")}}function R(){l++;const e=(l-1)*n,t=l*n;b(o,e,t),$(l,n,o)}F.addEventListener("submit",function(e){e.preventDefault(),o=E(m),y(o,n)});document.querySelectorAll(".container-filters").forEach(e=>{e.addEventListener("click",function(){e.classList.toggle("active-filter")})});document.addEventListener("click",function(e){const t=e.target.closest(".campers-link");if(!t)return;const i=t.dataset.id;I(i)});p.addEventListener("click",R);window.onload=N;
+    `}async function I(e){try{const t=await C(e);if(!t){console.error("Camper not found");return}const i=document.querySelector(".modal-content");if(!i){console.error("Modal content not found in DOM");return}i.innerHTML=M(t),document.querySelector(".modal").classList.add("open"),addNavigationHandlers()}catch(t){console.error("Error fetching camper data:",t)}}function T(){document.querySelector(".modal").classList.remove("open")}document.addEventListener("click",function(e){(e.target.classList.contains("close-modal")||e.target.classList.contains("modal"))&&T()});document.addEventListener("click",function(e){if(e.target.closest(".btn-like")){const t=e.target.closest(".btn-like"),i=t.dataset.id;t.classList.toggle("liked");const r=t.querySelector("img"),s=t.classList.contains("liked");r.src=s?"red-heart.svg":"heart.svg";const a=JSON.parse(localStorage.getItem("likedCampers"))||{};a[i]=s,localStorage.setItem("likedCampers",JSON.stringify(a))}});const O=document.querySelector(".result"),A=document.querySelector(".loader"),u=document.querySelector(".load-more"),F=document.querySelector("#filtersForm");A.style.display="none";u.style.display="none";let l=1;const n=4;let p=[],o=[];async function N(){try{p=await k(),o=p,y(o,n),l=1,$(l,n,o)}catch{O.innerHTML="<p>Not found</p>",u.classList.add("no-active")}}function R(){l++;const e=(l-1)*n,t=l*n;b(o,e,t),$(l,n,o)}F.addEventListener("submit",function(e){e.preventDefault(),o=E(p),y(o,n)});document.querySelectorAll(".container-filters").forEach(e=>{e.addEventListener("click",function(){e.classList.toggle("active-filter")})});document.addEventListener("click",function(e){const t=e.target.closest(".campers-link");if(!t)return;const i=t.dataset.id;I(i)});u.addEventListener("click",R);window.onload=N;
 //# sourceMappingURL=catalog.js.map
