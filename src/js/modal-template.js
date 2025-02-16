9705f54df773ede0abf9dfcac793156082ded4ca
Oleksandra Stepanova <oleksandra.stepanova@gmail.com>
2025-02-16 22:24:33 +0100

    Update modal-template.js

--- src/js/modal-template.js
+++ src/js/modal-template.js
@@ -4,7 +4,7 @@ export function modalTemplate(camper) {
       <h2 class='title-camp'>${camper.name}</h2>
       <div class='wrapper-camp'>
           <div class='reviews-camp'>
-              <img src='./src/img/star.svg' alt="star" />
+              <img src='/src/img/star.svg' alt="star" />
               <p>${camper.rating}</p>
               ${
                 camper.reviews
@@ -13,7 +13,7 @@ export function modalTemplate(camper) {
               }
           </div>
           <div class='location-camp'>
-              <img src='./src/img/map.svg' alt="map" />
+              <img src='/src/img/map.svg' alt="map" />
               <p>${camper.location}</p>
           </div>
       </div>
@@ -45,7 +45,7 @@ export function modalTemplate(camper) {
       <div id="outlet">
           <h1>Features Page</h1><p>Features of camper ID: ${camper.id}</p>
       </div>
-      <img class='close-modal' src="./src/img/close.svg" alt="close" width='22px' />
+      <img class='close-modal' src="/src/img/close.svg" alt="close" width='22px' />
     </section>
     `;
 }
