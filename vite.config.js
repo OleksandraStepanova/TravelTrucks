44971a97de1ccf3455dd2117141bb54e5805238c
Oleksandra Stepanova <oleksandra.stepanova@gmail.com>
2025-02-16 19:51:23 +0100

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,50 @@
 import { defineConfig } from 'vite';
+import * as glob from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({
-  server: {
-    historyApiFallback: true,
-  },
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
+      },
+      outDir: '../dist',
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
 });
-
 // import { defineConfig } from 'vite';
 
 // export default defineConfig({
-//   build: {
-//     rollupOptions: {
-//       input: {
-//         main: 'index.html',
-//         catalog: 'catalog.html',
-//       },
-//     },
+//   server: {
+//     historyApiFallback: true,
 //   },
 // });
+
+// // import { defineConfig } from 'vite';
+
+// // export default defineConfig({
+// //   build: {
+// //     rollupOptions: {
+// //       input: {
+// //         main: 'index.html',
+// //         catalog: 'catalog.html',
+// //       },
+// //     },
+// //   },
+// // });
