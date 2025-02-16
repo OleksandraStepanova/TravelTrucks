ba9befaf3adfd8d187e3300049ace4abbcf1f04f
Oleksandra Stepanova <oleksandra.stepanova@gmail.com>
2025-02-16 21:53:07 +0100

    redirect

--- src/main.js
+++ src/main.js
@@ -59,13 +59,13 @@ document.addEventListener('click', function (event) {
   const showMoreButton = event.target.closest('.campers-link');
   if (!showMoreButton) return;
   const camperId = showMoreButton.dataset.id;
-  window.history.pushState({}, '', `/catalog/${camperId}`);
+  window.history.pushState({}, '', `/catalog.html/${camperId}`);
   openModal(camperId);
 });
 
 document.addEventListener('DOMContentLoaded', () => {
   const pathParts = window.location.pathname.split('/').filter(Boolean);
-  const isCatalogPage = pathParts[0] === 'catalog';
+  const isCatalogPage = pathParts[0] === 'catalog.html';
   const camperId = isCatalogPage && pathParts[1] ? pathParts[1] : null;
 
   const savedCamperId = sessionStorage.getItem('openModal');
