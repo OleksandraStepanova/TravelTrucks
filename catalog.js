5f9f4b5f8df4ed1ddf2d5918bf7d7836157daf6a
OleksandraStepanova <128746781+OleksandraStepanova@users.noreply.github.com>
2025-02-17 00:54:32 +0000

    Deploying to gh-pages from @ OleksandraStepanova/TravelTrucks@56b75223bafec2f3fecbe466d186d88144a46692 🚀

--- catalog.js
+++ catalog.js
@@ -1,4 +1,4 @@
-/* empty css                      */import{a as v}from"./assets/vendor-BcYoTxAJ.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const i of document.querySelectorAll('link[rel="modulepreload"]'))c(i);new MutationObserver(i=>{for(const a of i)if(a.type==="childList")for(const r of a.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&c(r)}).observe(document,{childList:!0,subtree:!0});function s(i){const a={};return i.integrity&&(a.integrity=i.integrity),i.referrerPolicy&&(a.referrerPolicy=i.referrerPolicy),i.crossOrigin==="use-credentials"?a.credentials="include":i.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function c(i){if(i.ep)return;i.ep=!0;const a=s(i);fetch(i.href,a)}})();const f="https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers",d=document.querySelector(".loader");async function q(){try{d.style.display="block";const e=await v.get(f);return d.style.display="none",e.data.items}catch(e){throw console.error("Error:",e),e}}async function p(e){try{d.style.display="block";const t=await v.get(`${f}/${e}`);return d.style.display="none",t.data}catch(t){throw console.error("Error:",t),t}}function h(e){return`<li class="campers-item">
+/* empty css                      */import{a as v,i as C}from"./assets/vendor-CT17xhyR.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))a(s);new MutationObserver(s=>{for(const c of s)if(c.type==="childList")for(const r of c.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&a(r)}).observe(document,{childList:!0,subtree:!0});function i(s){const c={};return s.integrity&&(c.integrity=s.integrity),s.referrerPolicy&&(c.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?c.credentials="include":s.crossOrigin==="anonymous"?c.credentials="omit":c.credentials="same-origin",c}function a(s){if(s.ep)return;s.ep=!0;const c=i(s);fetch(s.href,c)}})();const f="https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers",d=document.querySelector(".loader");async function E(){try{d.style.display="block";const e=await v.get(f);return d.style.display="none",e.data.items}catch(e){throw console.error("Error:",e),e}}async function p(e){try{d.style.display="block";const t=await v.get(`${f}/${e}`);return d.style.display="none",t.data}catch(t){throw console.error("Error:",t),t}}function h(e){return`<li class="campers-item">
               <img class="img-campers" src="${e.gallery[0].original}" alt="${e.name}" />
               <div class="content-campers">
                   <div class="title-campers">
@@ -62,7 +62,28 @@
                   </ul>                    
                   <a class="campers-link" data-id="${e.id}">Show more</a>                    
               </div>
-          </li>`}const y=document.querySelector(".result"),b=document.querySelector(".load-more");function $(e,t){y.innerHTML=e.slice(0,t).map(h).join("");const s=JSON.parse(localStorage.getItem("likedCampers"))||{};document.querySelectorAll(".btn-like").forEach(c=>{const i=c.dataset.id;s[i]&&(c.classList.add("liked"),c.querySelector("img").src="red-heart.svg")})}function E(e,t,s){y.insertAdjacentHTML("beforeend",e.slice(t,s).map(h).join(""))}function w(e,t,s){b.style.display=e*t>=s.length?"none":"block"}function T(e){var i,a;const t=((i=document.querySelector("#location"))==null?void 0:i.value.toLowerCase())||"",s=[...document.querySelectorAll(".container-filters.active-filter")].map(r=>r.id),c=((a=document.querySelector('input[name="vehicleType"]:checked'))==null?void 0:a.value)||"";return e.filter(r=>{const k=t?r.location.toLowerCase().includes(t):!0,S=s.every(g=>r[g]===!0||r[g]==="automatic"),C=c?r.form===c:!0;return k&&S&&C})}async function L(e){const t=await p(e);return`<section class='features'>
+          </li>`}const y=document.querySelector(".result"),T=document.querySelector(".load-more");function $(e,t){y.innerHTML=e.slice(0,t).map(h).join("");const i=JSON.parse(localStorage.getItem("likedCampers"))||{};document.querySelectorAll(".btn-like").forEach(a=>{const s=a.dataset.id;i[s]&&(a.classList.add("liked"),a.querySelector("img").src="red-heart.svg")})}function A(e,t,i){y.insertAdjacentHTML("beforeend",e.slice(t,i).map(h).join(""))}function w(e,t,i){T.style.display=e*t>=i.length?"none":"block"}function M(e){var s,c;const t=((s=document.querySelector("#location"))==null?void 0:s.value.toLowerCase())||"",i=[...document.querySelectorAll(".container-filters.active-filter")].map(r=>r.id),a=((c=document.querySelector('input[name="vehicleType"]:checked'))==null?void 0:c.value)||"";return e.filter(r=>{const b=t?r.location.toLowerCase().includes(t):!0,S=i.every(g=>r[g]===!0||r[g]==="automatic"),q=a?r.form===a:!0;return b&&S&&q})}function k(){return`
+   <div class="form-container">
+        <h2 class='title-form'>Book your campervan now</h2>
+        <p class='text-form'>Stay connected! We are always ready to help you.</p>
+        <form id="bookingForm" class="form-bookings">
+            <div>    
+                <input class='input-bookings' type="text" id="name" name="name" required minlength="3" maxlength="50" placeholder="Name*" />
+            </div>
+            <div>    
+                <input class='input-bookings' type="email" id="email" name="email" required placeholder="Email*" />
+             </div>
+             <div>
+                <input class='input-bookings' type="date" id="bookingDate" name="bookingDate" required />
+            </div>
+            <div>    
+                <textarea class='comment-bookings' id="comment" name="comment" maxlength="250" placeholder="Comment"></textarea>
+            </div>
+            <button class='button-bookings' type="submit">Send</button>
+        </form>
+        
+      <div id="toast"></div>     
+   </div>`}async function L(e){const t=await p(e),i=k();return`<section class='features'>
             ${t?`<div>
                 <div class='info-feature'>
                     <ul class="characteristics-list">
@@ -131,8 +152,10 @@
                         </li>
                     </ul>
                 </div>
-            </div>`:""}            
-        </section>`}async function A(e){const t=await L(e.id);return`
+            </div>`:""} 
+            
+        ${i}
+        </section>`}async function I(e){const t=await L(e.id);return`
     <section class='camp'>
       <h2 class='title-camp'>${e.name}</h2>
       <div class='wrapper-camp'>
@@ -149,9 +172,9 @@
       <p class='price-camp'>&euro;${e.price},00</p>
       <div class='container-camp'>
           <ul class='gallery-camp'>
-              ${e.gallery.map((s,c)=>`
+              ${e.gallery.map((i,a)=>`
                 <li>
-                  <img class='image-camp' src=${s.original} alt="image ${c+1}" />
+                  <img class='image-camp' src=${i.original} alt="image ${a+1}" />
                 </li>
               `).join("")}
           </ul>
@@ -166,23 +189,23 @@
       </div>
       <img class='close-modal' src="close.svg" alt="close" width='22px' />
     </section>
-    `}async function M(e){const t=await p(e);return` <section class='section-reviews'>
+    `}async function x(e){const t=await p(e),i=k();return` <section class='section-reviews'>
             ${t?`<ul class='reviews-list'>
-                ${t.reviews.map(s=>`<li key=${s.reviewer_name} class='reviews-item'>
+                ${t.reviews.map(a=>`<li key=${a.reviewer_name} class='reviews-item'>
                     <div class='info-reviews'>
-                        <div class='ava-reviews'>${s.reviewer_name}</div>
+                        <div class='ava-reviews'>${a.reviewer_name}</div>
                         <div class='rating-reviews'>
-                            <p class='name-reviews'>${s.reviewer_name}</p>
+                            <p class='name-reviews'>${a.reviewer_name}</p>
                             <div class='rating-stars'>
-                             ${Array.from({length:s.reviewer_rating},()=>`
+                             ${Array.from({length:a.reviewer_rating},()=>`
                               <img src='star.svg' alt='star' />
                             `).join("")}
                             <div/>                                                
                         </div>
                     </div>  
-                    <p class='comment-reviews'>${s.comment}</p>
+                    <p class='comment-reviews'>${a.comment}</p>
                     </li>`).join("")}             
             </ul>`:""}
-          
-        </section>`}async function I(e){document.body.classList.add("no-scroll");try{const t=await p(e);if(!t){console.error("Camper not found");return}const s=document.querySelector(".modal-content");if(!s){console.error("Modal content not found in DOM");return}s.innerHTML=await A(t),document.querySelector(".modal").classList.add("open"),R()}catch(t){console.error("Error fetching camper data:",t)}}function O(){document.body.classList.remove("no-scroll"),document.querySelector(".modal").classList.remove("open")}document.addEventListener("click",function(e){(e.target.classList.contains("close-modal")||e.target.classList.contains("modal"))&&O()});async function N(e,t){const s={features:await L(t),reviews:await M(t)},c=document.getElementById("outlet");c&&(c.innerHTML=s[e]||"<h1>Catalog</h1>"),document.querySelectorAll(".nav-link").forEach(a=>a.classList.remove("active"));const i=document.querySelector(`.nav-link[data-page="${e}"]`);i&&i.classList.add("active")}function R(){document.querySelectorAll(".nav-link").forEach(e=>{e.addEventListener("click",function(t){t.preventDefault();const s=t.target.dataset.id;N(t.target.dataset.page,s)})})}document.addEventListener("click",function(e){if(e.target.closest(".btn-like")){const t=e.target.closest(".btn-like"),s=t.dataset.id;t.classList.toggle("liked");const c=t.querySelector("img"),i=t.classList.contains("liked");c.src=i?"red-heart.svg":"heart.svg";const a=JSON.parse(localStorage.getItem("likedCampers"))||{};a[s]=i,localStorage.setItem("likedCampers",JSON.stringify(a))}});const B=document.querySelector(".result"),F=document.querySelector(".loader"),u=document.querySelector(".load-more"),H=document.querySelector("#filtersForm");F.style.display="none";u.style.display="none";let o=1;const n=4;let m=[],l=[];async function _(){try{m=await q(),l=m,$(l,n),o=1,w(o,n,l)}catch{B.innerHTML="<p>Not found</p>",u.classList.add("no-active")}}function j(){o++;const e=(o-1)*n,t=o*n;E(l,e,t),w(o,n,l)}H.addEventListener("submit",function(e){e.preventDefault(),l=T(m),$(l,n)});document.querySelectorAll(".container-filters").forEach(e=>{e.addEventListener("click",function(){e.classList.toggle("active-filter")})});document.addEventListener("click",function(e){const t=e.target.closest(".campers-link");if(!t)return;const s=t.dataset.id;I(s)});u.addEventListener("click",j);window.onload=_;
+          ${i}
+        </section>`}async function F(e){document.body.classList.add("no-scroll");try{const t=await p(e);if(!t){console.error("Camper not found");return}const i=document.querySelector(".modal-content");if(!i){console.error("Modal content not found in DOM");return}i.innerHTML=await I(t),document.querySelector(".modal").classList.add("open"),R();const a=document.querySelector("#bookingForm");a.addEventListener("submit",function(s){s.preventDefault(),C.show({message:"Success! We have received your booking request. We will contact you shortly.",timeout:5e3,position:"topRight",color:"green"}),a.reset()})}catch(t){console.error("Error fetching camper data:",t)}}function O(){document.body.classList.remove("no-scroll"),document.querySelector(".modal").classList.remove("open")}document.addEventListener("click",function(e){(e.target.classList.contains("close-modal")||e.target.classList.contains("modal"))&&O()});async function N(e,t){const i={features:await L(t),reviews:await x(t)},a=document.getElementById("outlet");a&&(a.innerHTML=i[e]||"<p>Page not found</p>"),document.querySelectorAll(".nav-link").forEach(c=>c.classList.remove("active"));const s=document.querySelector(`.nav-link[data-page="${e}"]`);s&&s.classList.add("active")}function R(){document.querySelectorAll(".nav-link").forEach(e=>{e.addEventListener("click",function(t){t.preventDefault();const i=t.target.dataset.id;N(t.target.dataset.page,i)})})}document.addEventListener("click",function(e){if(e.target.closest(".btn-like")){const t=e.target.closest(".btn-like"),i=t.dataset.id;t.classList.toggle("liked");const a=t.querySelector("img"),s=t.classList.contains("liked");a.src=s?"red-heart.svg":"heart.svg";const c=JSON.parse(localStorage.getItem("likedCampers"))||{};c[i]=s,localStorage.setItem("likedCampers",JSON.stringify(c))}});const B=document.querySelector(".result"),H=document.querySelector(".loader"),u=document.querySelector(".load-more"),_=document.querySelector("#filtersForm");H.style.display="none";u.style.display="none";let o=1;const n=4;let m=[],l=[];async function j(){try{m=await E(),l=m,$(l,n),o=1,w(o,n,l)}catch{B.innerHTML="<p>Not found</p>",u.classList.add("no-active")}}function D(){o++;const e=(o-1)*n,t=o*n;A(l,e,t),w(o,n,l)}_.addEventListener("submit",function(e){e.preventDefault(),l=M(m),$(l,n)});document.querySelectorAll(".container-filters").forEach(e=>{e.addEventListener("click",function(){e.classList.toggle("active-filter")})});document.addEventListener("click",function(e){const t=e.target.closest(".campers-link");if(!t)return;const i=t.dataset.id;F(i)});u.addEventListener("click",D);window.onload=j;
 //# sourceMappingURL=catalog.js.map
